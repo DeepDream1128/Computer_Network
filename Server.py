af00d3bdc02bc4649a2dbbbe4008596e985a5efc
DeepDream1128 <46296633+DeepDream1128@users.noreply.github.com>
2023-12-12 12:13:15 +0000

    new file:   Server.py 	modified:   Task04/tcp.py 	new file:   Task07/receive.py 	new file:   Task07/send.py 	new file:   Task07/yolov5s.pt 	new file:   rev.pt

---
+++ Server.py
@@ -0,0 +1,15 @@
+import socket
+
+def start_server(port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('127.0.0.1', port))
+    server_socket.listen(5)
+    print(f"Listening on port {port}...")
+
+    while True:
+        client_socket, addr = server_socket.accept()
+        print(f"Accepted connection from {addr}")
+        client_socket.close()
+
+if __name__ == "__main__":
+    start_server(8080)
--- Task04/tcp.py
+++ Task04/tcp.py
@@ -1,4 +1,5 @@
 from scapy.all import *
+import time 
 
 def tcp_handshake(target_ip, target_port):
     # 构造SYN报文
@@ -31,8 +32,10 @@ def tcp_teardown(target_ip, target_port, seq, ack_seq):
         print("TCP四次挥手完成")
 
 if __name__ == "__main__":
-    target_ip = "www.google.com"  # 目标服务器IP
-    target_port = 80           # 目标服务器端口
+    target_ip = "127.0.0.1"  # 目标服务器IP
+    target_port = 8080              # 目标服务器端口
     seq, ack_seq = tcp_handshake(target_ip, target_port)
+
     if seq and ack_seq:
+        time.sleep(2)  # 等待一段时间再开始挥手
         tcp_teardown(target_ip, target_port, seq, ack_seq)
