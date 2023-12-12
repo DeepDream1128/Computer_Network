f199d24ae072a44119dd55c75ab3fc6251da0aa8
DeepDream1128 <1076262111@qq.com>
2023-12-12 20:53:28 +0800

    新文件：   .gitignore 	新文件：   LICENSE 	新文件：   Server.py 	新文件：   Task01/subnet.py 	新文件：   Task02/TCP.py 	新文件：   Task03/Traceroute.py 	新文件：   Task04/tcp.py 	新文件：   Task05/ip.py 	新文件：   Task06/dnss.py 	新文件：   Task07/receive.py 	新文件：   Task07/send.py 	新文件：   Task07/yolov5s.pt 	新文件：   rev.pt

---
+++ Task05/ip.py
@@ -0,0 +1,31 @@
+from scapy.all import *
+import sys
+from concurrent.futures import ThreadPoolExecutor
+
+def scan_port(ip, port):
+    try:
+        # TCP SYN Scan
+        syn_pkt = IP(dst=ip) / TCP(dport=port, flags="S")
+        response = sr1(syn_pkt, timeout=1, verbose=0)
+        if response and response.haslayer(TCP) and response.getlayer(TCP).flags & 0x12:
+            return port, 'Open'
+
+        # UDP Scan
+        udp_pkt = IP(dst=ip) / UDP(dport=port)
+        response = sr1(udp_pkt, timeout=1, verbose=0)
+        if not response or (response.haslayer(ICMP) and response.getlayer(ICMP).type != 3):
+            return port, 'Open/Filtered'
+    except Exception as e:
+        return port, 'Error'
+    return port, 'Closed'
+
+def scan(ip, ports, max_threads=100):
+    with ThreadPoolExecutor(max_workers=max_threads) as executor:
+        scan_results = executor.map(lambda p: scan_port(ip, p), ports)
+        for port, status in scan_results:
+            print(f"Port {port}: {status}")
+
+if __name__ == "__main__":
+    target_ip = sys.argv[1] if len(sys.argv) > 1 else "218.193.144.94"
+    ports = range(1, 1025)  # Standard range of ports
+    scan(target_ip, ports)
